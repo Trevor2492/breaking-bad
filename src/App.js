fe332b21c629d5726bb55c1afc0049925ebbf807
Trevor Allen <trevor2492@gmail.com>
2021-04-07 13:16:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
+import Search from './components/ui/Search';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [ items, setItems ] = useState([])
+  const [ isLoading, setIsLoading ] = useState(true)
+  const [ query, setQuery ] = useState('')
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`) //This endpoint comes from https://breakingbadapi.com/documentation
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [query]) //If this 'dependency' is empty, useEffect will only fire once at the beginning when the app is loaded the first time
+              //With this 'query' dependency, it will fire any time 'query' changes
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} items={items}/>
     </div>
   );
 }
